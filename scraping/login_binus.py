6025b2d541e753bbe1c8d51ec48cef448c522d34
jimmy ready <jimmyready>
2024-02-17 18:42:05 +0700

    chore: implementation django

--- scraping/login_binus.py
+++ scraping/login_binus.py
@@ -3,8 +3,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-from config import emailBNS, passwordBNS
-from time import sleep
+from application.config.settings import emailBNS, passwordBNS
 
 
 def loginBinus(driver: webdriver) -> bool:
