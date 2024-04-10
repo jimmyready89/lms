f381032d94f6f6f4cd9c5627e106c050474ecdc9
WieElang <elangsurya66@gmail.com>
2024-04-11 00:48:26 +0700

    fix: add course relation

--- application/apps/group/admin.py
+++ application/apps/group/admin.py
@@ -3,8 +3,13 @@ from .models import Group
 
 
 class GroupAdmin(admin.ModelAdmin):
-    list_display = ['number']
-    search_fields = ['number']
+    list_display = ['number', 'get_course']
+    search_fields = ['get_course']
+    list_select_related = ['course']
+
+    @admin.display(ordering='course', description='Course')
+    def get_course(self, obj):
+        return obj.course.name
 
 
 admin.site.register(Group, GroupAdmin)
--- application/apps/group/models.py
+++ application/apps/group/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class Group(models.Model):
     number = models.IntegerField()
+    course = models.ForeignKey('courses.Courses', related_name='groups', on_delete=models.CASCADE)
     peoples = models.ManyToManyField('people.People', related_name='groups')
 
     def __str__(self) -> str:
-        return f"Group {self.number}"
+        return f"Group {self.number} in ({self.course.name})"
